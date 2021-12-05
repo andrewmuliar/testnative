30471656d277c7bccec6e8420c8d2f83a60f4997
amular <amular@itstudiodev.com>
2021-12-05 18:16:37 +0200

    mib version

--- libs/libtest/src/lib/todos/todos.component.ts
+++ libs/libtest/src/lib/todos/todos.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'test-todos',
-  templateUrl: './todos.component.web.html',
+  templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
